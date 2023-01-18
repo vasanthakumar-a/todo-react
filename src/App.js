89739f51df25d55e0129abe6a5fe3dfdf45df0d6
Vasanthakumar <vasanthakumara117@gmail.com>
2023-01-18 23:17:59 +0530

    displays the data in rails model through axios api

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
+import axios from 'axios';
+
+const API_URL = "http://localhost:3000/api/v1/todo_lists"
+
+function getAPIData() {
+  return axios.get(API_URL).then((response) => response.data)
+}
 
 function App() {
+  const [todoLists,setTodoLists] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getAPIData().then((items) => {
+      if(mounted) {
+        setTodoLists(items);
+      }
+    });
+    return () => (mounted = false);
+  });
+
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState("all");
@@ -31,6 +50,17 @@ function App() {
       <header>
         <h1>Vasi's Todo List</h1>
       </header>
+
+      <h4>Here are Todos from rails</h4>
+      {todoLists.map((todo_item) => {
+        return (
+          <div key={todo_item.id}>
+            <h5>{todo_item.todo_item}</h5>
+            <p>{todo_item.status}</p>
+          </div>
+        )
+      })}
+
       <Form todos={todos} setTodos={setTodos} inputText={inputText} setInputText={setInputText} setStatus={setStatus} />
       <TodoList filteredTodos={filteredTodos} todos={todos} setTodos={setTodos} />
     </div>
